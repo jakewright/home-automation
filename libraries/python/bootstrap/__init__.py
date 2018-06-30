bac66d0f399f96cfab8cab25945da44c39c346ed
Jake Wright <jake@jakewright.net>
2018-06-30 22:31:39 +0100

    Add service bootstrap Python library

---
+++ libraries/python/bootstrap/__init__.py
@@ -0,0 +1,37 @@
+import markdown
+import os
+from flask import Flask, g, Markup, render_template
+from flask_restful import Resource, Api, reqparse
+
+class Service:
+    def __init__(self):
+        # Create the application with the instance config option on
+        self.app = Flask(__name__, instance_relative_config=True)
+
+        # Load the default configuration
+        self.app.config.from_object('config.default')
+
+        # Load the file specified by the APP_CONFIG_FILE environment variable
+        # Variables defined here will override those in the default configuration
+        self.app.config.from_envvar('APP_CONFIG_FILE')
+
+        self.app.add_url_rule('/', 'index', self.present_documentation)
+
+        # Create the API
+        self.api = Api(self.app)
+
+    def serve(self):
+        self.app.run(host='0.0.0.0', port=int(self.app.config['PORT']), debug=True)
+
+    def present_documentation(self):
+        """Present some documentation."""
+
+        # Open the README file
+        with open('./README.md', 'r') as markdown_file:
+            # Read the markdown contents
+            content = markdown_file.read()
+
+            # Convert the markdown to HTML and then treat it as actual HTML so it's not escaped
+            html = Markup(markdown.markdown(content, extensions=['markdown.extensions.fenced_code']))
+
+        return render_template('index.html', content=html)
