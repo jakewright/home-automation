5afeec3dab960311b3f5fdd5fe447a422ea6108d
Jake Wright <jake@jakewright.net>
2018-09-26 22:46:06 +0100

    Refactor Hue service

--- service.controller.hue/domain/ColorTempDecorator.js
+++ service.controller.hue/domain/ColorTempDecorator.js
@@ -1,65 +1,33 @@
 const CT_MIN = 2000;
 const CT_MAX = 6536;
 
-const ColorTempDecorator = hueLight => {
-  hueLight.colorTemp = 2700;
+const colorTempDecorator = {
+  transform(state, t) {
+    if ("colorTemp" in state) {
+      if (state.colorTemp < CT_MIN || state.colorTemp > CT_MAX)
+        throw new Error(`Invalid colour temperature '${state.colorTemp}'`);
 
-  hueLight.setColorTemp = temp => {
-    if (hueLight.colorTemp === temp) return;
-
-    // Despite what the docs say, huejay sometimes returns numbers higher than 6500
-    if (temp < CT_MIN || temp > CT_MAX) {
-      throw new Error(`Invalid colour temperature "${temp}"`);
+      // Convert from Kelvin to Mirek (Huejay wants a valye between 153 and 500)
+      t.colorTemp = Math.floor(1000000 / state.colorTemp);
+      t.on = true;
     }
 
-    hueLight.colorTemp = temp;
-    hueLight.power = true;
-  };
+    return t;
+  },
 
-  const setState = hueLight.setState;
-  hueLight.setState = state => {
-    setState.call(hueLight, state);
-    if ("colorTemp" in state) hueLight.setColorTemp(state.colorTemp);
-  };
+  applyRemoteState({ colorTemp }) {
+    this.colorTemp = Math.ceil(1000000 / colorTemp);
+  },
 
-  const prepareLight = hueLight.prepareLight;
-  hueLight.prepareLight = () => {
-    prepareLight.call(hueLight);
-
-    // Don't try to set colorTemp if the light is off
-    if (!hueLight.power) return;
-
-    // Don't try to set colorTemp if the value hasn't changed
-    if (hueLight.cache.colorTemp === hueLight.colorTemp) return;
-
-    // Convert from Kelvin to Mirek (Huejay wants a valye between 153 and 500)
-    hueLight.light.colorTemp = Math.floor(1000000 / hueLight.colorTemp);
-  };
-
-  const applyRemoteState = hueLight.applyRemoteState;
-  hueLight.applyRemoteState = light => {
-    hueLight.setColorTemp(Math.ceil(1000000 / light.colorTemp));
-    applyRemoteState.call(hueLight, light);
-  };
-
-  const getProperties = hueLight.getProperties;
-  hueLight.getProperties = () => {
-    let properties = getProperties.call(hueLight);
-    properties["colorTemp"] = {
+  getProperties(properties) {
+    properties.colorTemp = {
       type: "int",
       min: CT_MIN,
       max: CT_MAX,
       interpolation: "continuous"
     };
     return properties;
-  };
-
-  const toJSON = hueLight.toJSON;
-  hueLight.toJSON = () => {
-    let json = toJSON.call(hueLight);
-    json["colorTemp"] = hueLight.colorTemp;
-    return json;
-  };
+  }
 };
 
-exports = module.exports = ColorTempDecorator;
+exports = module.exports = colorTempDecorator;
