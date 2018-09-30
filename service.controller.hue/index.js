8815117f8f74d10ee1e2bcd12a30d293e67bc7c9
Jake Wright <jake@jakewright.net>
2018-09-30 23:30:45 +0100

    Use config from config service

--- service.controller.hue/index.js
+++ service.controller.hue/index.js
@@ -6,70 +6,77 @@ const colorTempDecorator = require("./domain/colorTempDecorator");
 const rgbDecorator = require("./domain/rgbDecorator");
 const hueClient = require("./api/hueClient");
 const DeviceController = require("./controllers/DeviceController");
-const HueDiscoveryController = require("./controllers/HueDiscoveryController");
+const HueDiscoveryController = require("./controllers/HueBridgeController");
+const HueBridgeController = require("./controllers/HueBridgeController");
 const store = require("./store/devices");
 
 /* Use the bootstrap library to create a Service object */
-const service = new Service();
+const service = new Service("service.controller.hue");
+service.init()
+  .then(() => {
+    hueClient.setConfig({
+      host: service.config.get("hueBridge.host"),
+      username: service.config.get("hueBridge.username"),
+    });
 
-hueClient.setConfig({
-  host: service.config.get("hueBridge.host"),
-  username: service.config.get("hueBridge.username")
-});
-
-/* Subscribe to state changes from the store */
-store.on("key-changed", key => {
-  const device = store.get("device", key);
-  console.log(`State changed for device ${device.identifier}`);
-  service.redisClient.publish(
-    `device-state-changed.${device.identifier}`,
-    JSON.stringify(device)
-  );
-});
-
-/* Initialise the devices */
-service.apiClient
-  .getDevices(service.controllerName)
-  .then(deviceHeaders => {
-    for (header of deviceHeaders) {
-      const device = instantiateDevice(header);
-      console.log(`Controlling light '${device.identifier}'`);
-      store.commit("setDevice", device);
-    }
-  })
-  .catch(err => {
-    console.error("Failed to intialise devices:", err);
-  });
-
-/* Initialise controller to add routes */
-new DeviceController(service.app, store);
-
-/* Add an error handler that returns valid JSON */
-service.app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500);
-  res.json({
-    message: "An error occurred",
-    error: err.message
-  });
-});
-
-/* Start the server */
-service.listen();
-
-/* Poll for state changes */
-let pollingTimer = setInterval(() => {
-  const devices = store.get("devices");
-  for (let id in devices) {
-    console.log(`Refreshing state for '${devices[id].identifier}'`);
-    store.dispatch("fetchDevice", devices[id]).catch(err => {
-      console.error(
-        `Failed to refresh state for '${devices[id].identifier}':`,
-        err
+    /* Subscribe to state changes from the store */
+    store.on("key-changed", key => {
+      const device = store.get("device", key);
+      console.log(`State changed for device ${device.identifier}`);
+      service.redisClient.publish(
+        `device-state-changed.${device.identifier}`,
+        JSON.stringify(device)
       );
     });
-  }
-}, 5000);
+
+    /* Initialise the devices */
+    service.apiClient
+      .get("service.registry.device/devices", { controllerName: service.controllerName })
+      .then(deviceHeaders => {
+        for (header of deviceHeaders) {
+          const device = instantiateDevice(header);
+          console.log(`Controlling light '${device.identifier}'`);
+          store.commit("setDevice", device);
+        }
+      })
+      .catch(err => {
+        console.error("Failed to intialise devices:", err);
+      });
+
+    /* Initialise controller to add routes */
+    new DeviceController(service.app, store);
+    new HueBridgeController(service.app, hueClient);
+
+    /* Add an error handler that returns valid JSON */
+    service.app.use(function(err, req, res, next) {
+      console.error(err.stack);
+      res.status(500);
+      res.json({message: err.message});
+    });
+
+    /* Start the server */
+    service.listen();
+
+    /* Poll for state changes */
+    if (service.config.get("polling.enabled", false)) {
+      console.log("Polling for state changes");
+
+      let pollingTimer = setInterval(() => {
+        const devices = store.get("devices");
+        for (let id in devices) {
+          console.log(`Refreshing state for '${devices[id].identifier}'`);
+          store.dispatch("fetchDevice", devices[id]).catch(err => {
+            console.error(
+              `Failed to refresh state for '${devices[id].identifier}':`,
+              err
+            );
+          });
+        }
+      }, service.config.get("polling.interval", 30000));
+    }
+  }).catch(err => {
+    console.error("Error initialising service", err);
+  });
 
 const instantiateDevice = header => {
   let device = new HueLight({
