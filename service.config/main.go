6b5a61be5e6f9d05e5d78f0a448d2f443c19e332
Jake Wright <jake@jakewright.net>
2018-09-30 16:47:12 +0100

    Yo dawg I heard you like config so service.config now has config

--- service.config/main.go
+++ service.config/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"home-automation/libraries/go/muxinator"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/jakewright/muxinator"
 	"home-automation/service.config/controller"
 	"home-automation/service.config/domain"
 	"home-automation/service.config/service"
-	"log"
-	"net/http"
-	"time"
 )
 
 func main() {
@@ -23,12 +24,34 @@ func main() {
 		ConfigService: &configService,
 	}
 
-	go configService.Watch(time.Second * 30)
+	_, err := configService.Reload()
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	selfConfig, err := config.Get("service.config")
+	if err != nil {
+		log.Fatalf("Error reading own config: %v", err)
+	}
+
+	if polling, ok := selfConfig["polling"].(map[string]interface{}); ok {
+		if enabled, ok := polling["enabled"]; ok && enabled.(bool) {
+			if interval, ok := polling["interval"].(int); ok {
+				log.Printf("Polling for config changes every %d milliseconds", interval)
+				go configService.Watch(time.Millisecond * time.Duration(interval))
+			}
+		}
+	}
 
 	router := muxinator.NewRouter()
 	router.Get("/read/{serviceName}", c.ReadConfig)
 	router.Patch("/reload", c.ReloadConfig)
 
-	log.Println("Listening on port 80")
-	log.Fatal(http.ListenAndServe(":80", router.BuildHandler()))
+	port, ok := selfConfig["port"].(int)
+	if !ok {
+		port = 80
+	}
+
+	log.Printf("Listening on port %d\n", port)
+	log.Fatal(router.ListenAndServe(fmt.Sprintf(":%d", port)))
 }
