0a3195dc5eb8b0787e6edfd042d76b077fe1fd6a
Jake Wright <jake@jakewright.net>
2019-02-01 22:46:23 +0000

    Add status flag to docker deploy scripts

--- tools/orchestrate/deploy.py
+++ tools/orchestrate/deploy.py
@@ -8,6 +8,7 @@ import subprocess
 # Parse arguments
 parser = argparse.ArgumentParser(description='Deploy services')
 parser.add_argument('service', help='The name of the service to deploy')
+parser.add_argument('--status', help='Get the currently deployed revision', action='store_true')
 args = parser.parse_args()
 
 # Read config
@@ -27,9 +28,19 @@ if config[args.service]['System'] != 'docker':
     print("Deployment system not supported")
     exit(1)
 
+if args.status:
+    subprocess.run(dir_path + "/docker/status.sh", shell=True, env={
+        "SERVICE": args.service,
+        "DEPLOYMENT_TARGET": config[args.service]['DeploymentTarget'],
+        "TARGET_USERNAME": config[args.service]['TargetUsername'],
+        "TARGET_DIRECTORY": config[args.service]['TargetDirectory'],
+    })
+    exit(0)
+
 subprocess.run(dir_path + "/docker/deploy.sh", shell=True, env={
     "SERVICE": args.service,
     "DEPLOYMENT_TARGET": config[args.service]['DeploymentTarget'],
     "TARGET_USERNAME": config[args.service]['TargetUsername'],
     "TARGET_DIRECTORY": config[args.service]['TargetDirectory'],
 })
+exit(0)
---
+++ tools/orchestrate/docker/status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Abort if anything fails
+set -e
+
+UNDERSCORES=$(echo $SERVICE | tr "." "_" | tr "-" "_")
+
+ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null "$TARGET_USERNAME"@"$DEPLOYMENT_TARGET" << EOF
+    cd $TARGET_DIRECTORY
+    REV=\$(cat .env | grep $UNDERSCORES | tail -1 | cut -d "=" -f 2)
+    echo "Current revision: \$REV"
+    docker ps --filter "name=$SERVICE" --format "{{.Image}} {{.Status}}"
+EOF
