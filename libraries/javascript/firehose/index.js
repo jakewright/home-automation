1b73d191db37f456243c4b8353d1a73e65967cf5
Jake Wright <jake@jakewright.net>
2019-02-16 23:34:15 +0000

    Create Firehose class

---
+++ libraries/javascript/firehose/index.js
@@ -0,0 +1,29 @@
+const redis = require("redis");
+const config = require("../config");
+
+class Firehose {
+  getClient() {
+    if (this.client === undefined) {
+      if (!config.has("redis.host")) {
+        throw new Error("No redis host defined in config");
+      }
+
+      this.client = redis.createClient({
+        host: config.get("redis.host"),
+        port: config.get("redis.port")
+      });
+      this.client.on("error", err => {
+        console.error(`Redis error: ${err}`);
+      })
+    }
+
+    return this.client;
+  }
+
+  publish(name, msg) {
+    this.getClient().publish(name, msg);
+  }
+}
+
+const firehose = new Firehose();
+exports = module.exports = firehose;
