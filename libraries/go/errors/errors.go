6d3969a45c8ec7b92c1f7164c849d59c8e3a712a
Jake Wright <jake@jakewright.net>
2019-04-14 19:10:25 +0100

    Add errors library

---
+++ libraries/go/errors/errors.go
@@ -0,0 +1,130 @@
+package errors
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// Error is a custom error type that implements Go's error interface
+type Error struct {
+	Code    string            `json:"code"`
+	Message string            `json:"message"`
+	Params  map[string]string `json:"params"`
+}
+
+// Error returns a string message of the error
+func (e *Error) Error() string {
+	switch {
+	case e == nil:
+		return ""
+	case e.Message == "":
+		return e.Code
+	case e.Code == "":
+		return e.Message
+	default:
+		return fmt.Sprintf("%s: %s", e.Code, e.Message)
+	}
+}
+
+// HTTPStatus returns an appropriate HTTP status code to use when returning the error in a response
+func (e *Error) HTTPStatus() int {
+	switch e.Code {
+	case ErrBadRequest:
+		return http.StatusBadRequest
+	case ErrForbidden:
+		return http.StatusForbidden
+	case ErrInternalService:
+		return http.StatusInternalServerError
+	case ErrNotFound:
+		return http.StatusNotFound
+	case ErrPreconditionFailed:
+		return http.StatusPreconditionFailed
+	case ErrTimeout:
+		return http.StatusRequestTimeout
+	case ErrUnauthorized:
+		return http.StatusUnauthorized
+	default:
+		return http.StatusInternalServerError
+	}
+}
+
+// Matches returns whether the string returned from error.Error() contains the given param string
+func (e *Error) Matches(match string) bool {
+	return strings.Contains(e.Error(), match)
+}
+
+// Generic error codes. Each of these has their own constructor for convenience.
+// You can use any string as a code, just use the `New` method.
+const (
+	ErrBadRequest         = "bad_request"
+	ErrForbidden          = "forbidden"
+	ErrInternalService    = "internal_service"
+	ErrNotFound           = "not_found"
+	ErrPreconditionFailed = "precondition_failed"
+	ErrTimeout            = "timeout"
+	ErrUnauthorized       = "unauthorized"
+)
+
+// InternalService creates a new error to represent an internal service error
+func InternalService(format string, a ...interface{}) *Error {
+	return newError(ErrInternalService, format, a)
+}
+
+// BadRequest creates a new error to represent an error caused by the client sending
+// an invalid request. This is non-retryable unless the request is modified.
+func BadRequest(format string, a ...interface{}) *Error {
+	return newError(ErrBadRequest, format, a)
+}
+
+// Forbidden creates a new error representing a resource that cannot be accessed with
+// the current authorisation credentials. The user may need authorising, or if authorised,
+// may not be permitted to perform this action.
+func Forbidden(format string, a ...interface{}) *Error {
+	return newError(ErrForbidden, format, a)
+}
+
+// NotFound creates a new error representing a resource that cannot be found
+func NotFound(format string, a ...interface{}) *Error {
+	return newError(ErrNotFound, format, a)
+}
+
+// PreconditionFailed creates a new error indicating that one or more conditions
+// given in the request evaluated to false when tested on the server
+func PreconditionFailed(format string, a ...interface{}) *Error {
+	return newError(ErrPreconditionFailed, format, a)
+}
+
+// Timeout creates a new error representing a timeout from client to server
+func Timeout(format string, a ...interface{}) *Error {
+	return newError(ErrTimeout, format, a)
+}
+
+// Unauthorized creates a new error indicating that authentication is required,
+// but has either failed or not been provided.
+func Unauthorized(format string, a ...interface{}) *Error {
+	return newError(ErrUnauthorized, format, a)
+}
+
+// newError returns a new Error with the given code. The message is formatted using Sprintf.
+// If the last parameter is a map[string]string, it is assumed to be the error params.
+func newError(code, format string, a ...interface{}) *Error {
+	// Take the last parameter
+	last := a[len(a)-1]
+
+	// Try to cast it to a map[string]string. If it fails, params will be an empty map.
+	params, ok := last.(map[string]string)
+
+	var message string
+
+	// If the last parameter was a map[string]string
+	if ok {
+		// Format the message using all but the last parameter
+		message = fmt.Sprintf(format, a[:len(a)-1])
+	} else {
+		// Format the string using all parameters
+		message = fmt.Sprintf(format, a)
+	}
+
+	return &Error{code, message, params}
+}
