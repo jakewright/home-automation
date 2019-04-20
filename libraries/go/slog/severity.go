07bfcd90ada370584d5f4986f7e8b7218b192b80
Jake Wright <jake@jakewright.net>
2019-04-20 10:26:10 +0100

     Represent severity as an int so they can be easily compared

--- libraries/go/slog/severity.go
+++ libraries/go/slog/severity.go
@@ -1,18 +1,33 @@
 package slog
 
 // Severity is a subset of the syslog severity levels
-type Severity string
+type Severity int
 
 const (
 	// DebugSeverity is the severity used for debug-level messages
-	DebugSeverity Severity = "DEBUG"
+	DebugSeverity Severity = 2
 
 	// InfoSeverity is the severity used for informational messages
-	InfoSeverity Severity = "INFO"
+	InfoSeverity Severity = 3
 
 	// WarnSeverity is the severity used for warning conditions
-	WarnSeverity Severity = "WARNING"
+	WarnSeverity Severity = 5
 
 	// ErrorSeverity is the severity used for error conditions
-	ErrorSeverity Severity = "ERR"
+	ErrorSeverity Severity = 6
 )
+
+func (s Severity) String() string {
+	switch s {
+	case DebugSeverity:
+		return "DEBUG"
+	case InfoSeverity:
+		return "INFO"
+	case WarnSeverity:
+		return "WARN"
+	case ErrorSeverity:
+		return "ERROR"
+	}
+
+	return "UNKNOWN"
+}
