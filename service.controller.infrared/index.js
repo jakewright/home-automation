1821bb16b9189ae0016646a7f6ad5daa7f392d80
Jake Wright <jake@jakewright.net>
2019-06-16 18:32:59 +0100

    Fetch state and listen on startup

--- service.controller.infrared/index.js
+++ service.controller.infrared/index.js
@@ -1,7 +1,16 @@
 const bootstrap = require("../libraries/javascript/bootstrap");
+const router = require("../libraries/javascript/router");
+const dao = require("./dao");
+require("./routes");
 
 const serviceName = "service.controller.infrared";
 bootstrap(serviceName)
+  .then(() => {
+    return dao.fetchAllState()
+  })
+  .then(() => {
+    router.listen();
+  })
   .catch(err => {
     console.error("Error initialising service", err)
   });
