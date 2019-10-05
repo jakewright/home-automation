7b76fc586bab14bdb401b4f363b2e04e085eda2a
Jake Wright <jake@jakewright.net>
2019-10-05 19:26:31 +0100

    Add color helper

---
+++ libraries/go/util/color.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"fmt"
+	"image/color"
+)
+
+func ParseHexColor(s string) (c color.RGBA, err error) {
+	c.A = 0xff
+	switch len(s) {
+	case 7:
+		_, err = fmt.Sscanf(s, "#%02x%02x%02x", &c.R, &c.G, &c.B)
+	case 4:
+		_, err = fmt.Sscanf(s, "#%1x%1x%1x", &c.R, &c.G, &c.B)
+		// Double the hex digits:
+		c.R *= 17
+		c.G *= 17
+		c.B *= 17
+	default:
+		err = fmt.Errorf("invalid length, must be 7 or 4")
+	}
+
+	return
+}
