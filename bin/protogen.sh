a6c4105e7d4d61d523149078860eac7ebc1fd703
Jake Wright <jake@jakewright.net>
2020-01-02 20:55:16 +0000

    goimports

--- bin/protogen.sh
+++ bin/protogen.sh
@@ -20,9 +20,11 @@ if [[ ! -x "$(command -v protoc-gen-jrpc)" ]]; then
     exit 1
 fi
 
-DIR=$(dirname "${BASH_SOURCE[0]}")
-ROOT=$(dirname "$DIR")
-ROOT_ABS="$( cd "$ROOT" ; pwd -P )"
+script_dir=$(dirname "${BASH_SOURCE[0]}")
+project_root=$(dirname "$script_dir")
+
+# This gets the absolute path instead of relative
+project_root="$( cd "$project_root" ; pwd -P )"
 
 # protoc does not properly work with go modules
 # and assumes that the project will be in your
@@ -30,9 +32,9 @@ ROOT_ABS="$( cd "$ROOT" ; pwd -P )"
 # GOPATH, the directory structure just has to match.
 # Check that the root directory ends with the right
 # pattern so imports work as expected.
-IMPORT_PATH="github.com/jakewright/home-automation"
-if [[ ! $ROOT_ABS == *"$IMPORT_PATH" ]]; then
-    printf "\033[41mProject needs to be in a path ending with $IMPORT_PATH\033[0m\n"
+import_path="github.com/jakewright/home-automation"
+if [[ ! $project_root == *"$import_path" ]]; then
+    printf "\033[41mProject needs to be in a path ending with $import_path\033[0m\n"
     exit 1
 fi
 
@@ -42,20 +44,20 @@ fi
 # import path to find the project's files.
 # For information about how this works, see:
 # https://www.tldp.org/LDP/abs/html/parameter-substitution.html
-SRC=${ROOT_ABS%$IMPORT_PATH}
+go_path=${project_root%$import_path}
 
 # Remove slash from beginning of service directory
-SVC_DIR=${1#/}
+service_dir=${1#/}
 
 # Create the full path to the service
-SVC_DIR="$ROOT_ABS/$SVC_DIR"
+service_dir="$project_root/$service_dir"
 
 # Remove slash from the end.
-SVC_DIR=${SVC_DIR%/}
+service_dir=${service_dir%/}
 
 # Make sure what we have ended up with is a directory
-if [[ ! -d "$SVC_DIR" ]]; then
-    printf "\033[41mCannot find directory at $SVC_DIR\033[0m"
+if [[ ! -d "$service_dir" ]]; then
+    printf "\033[41mCannot find directory at $service_dir\033[0m"
     exit 1
 fi
 
@@ -64,10 +66,19 @@ GREEN="\033[32m"
 RESET="\033[0m"
 
 # Generate all of the proto files we find
-FILES=$(find "$SVC_DIR" -maxdepth 3 -type f -name "*.proto")
-for f in $FILES; do
-    PRETTY_NAME=${f#$ROOT_ABS}
-    printf "Compiling $PRETTY_NAME ";
-    protoc --proto_path="$SRC" --go_out="$SRC" --jrpc_out="$SRC" $f
+files=$(find "$service_dir" -maxdepth 3 -type f -name "*.proto")
+for f in $files; do
+    # Strip the project_root from the filename
+    f_pretty=${f#$project_root}
+    printf "Compiling $f_pretty ";
+
+    # Generate the files
+    protoc --proto_path="$go_path" --go_out="$go_path" --jrpc_out="$go_path" $f
+
+    # Remove the ".proto" from the end of the filename
+    f_base=${f%".proto"}
+    
+    # Run goimports on all of the files we expect to have been generated
+    goimports -w "$f_base.pb.go" "$f_base.rpc.go"
     printf "$GREEN$TICK$RESET\n"
 done
