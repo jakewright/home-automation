dc02e1261735533809a31c42b95e2f70de12b77e
Jake Wright <jake@jakewright.net>
2020-02-24 23:49:55 +0000

    Define device-state-changed event

--- libraries/go/device/def/types.go
+++ libraries/go/device/def/types.go
@@ -2,6 +2,10 @@
 
 package devicedef
 
+import (
+	errors "github.com/jakewright/home-automation/libraries/go/errors"
+)
+
 // Device is defined in the .def file
 type Device struct {
 	Id             string                 `json:"id"`
@@ -23,6 +27,11 @@ type Property struct {
 	Interpolation string      `json:"interpolation"`
 }
 
+// DeviceStateChangedEvent is defined in the .def file
+type DeviceStateChangedEvent struct {
+	Device *Device `json:"device"`
+}
+
 // Validate returns an error if any of the fields have bad values
 func (m *Device) Validate() error {
 	return nil
@@ -30,5 +39,17 @@ func (m *Device) Validate() error {
 
 // Validate returns an error if any of the fields have bad values
 func (m *Property) Validate() error {
+	return nil
+}
+
+// Validate returns an error if any of the fields have bad values
+func (m *DeviceStateChangedEvent) Validate() error {
+	if err := m.Device.Validate(); err != nil {
+		return err
+	}
+
+	if m.Device == nil {
+		return errors.BadRequest("field device is required")
+	}
 	return nil
 }
