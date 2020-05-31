c12fda776447d03a4d6d7f056b6b05c434bc1318
Jake Wright <jake@jakewright.net>
2020-05-31 14:48:41 +0100

    Don't read config until provider is set

--- libraries/go/util/environment.go
+++ libraries/go/util/environment.go
@@ -4,9 +4,7 @@ import "github.com/jakewright/home-automation/libraries/go/config"
 
 const envProd = "prod"
 
-var env = config.Get("ENV").String(envProd)
-
 // IsProd returns whether the current environment is production, based on config.
 func IsProd() bool {
-	return env == envProd
+	return config.Get("ENV").String(envProd) == envProd
 }
