74e083cbb16cb3a7ffa4a3add82defccd23c0ea5
Jake Wright <jake@jakewright.net>
2020-06-04 01:18:45 +0100

    Remove config prefix

--- libraries/go/config/env.go
+++ libraries/go/config/env.go
@@ -6,11 +6,9 @@ import (
 	"github.com/jakewright/home-automation/libraries/go/slog"
 )
 
-const prefix = "HA"
-
 // Load populates the given struct with config from the environment
 func Load(conf interface{}) {
-	if err := envconfig.InitWithPrefix(conf, prefix); err != nil {
+	if err := envconfig.Init(conf); err != nil {
 		slog.Panicf("Failed to load config from environment: %v", err)
 	}
 }
