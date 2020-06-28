faf3ce53379515d108090258561eae4cafbc8f34
Jake Wright <jake@jakewright.net>
2020-06-28 19:28:53 +0100

    Export decoder function

---
+++ libraries/go/taxi/decode_test.go
@@ -0,0 +1,93 @@
+package taxi
+
+import (
+	"bytes"
+	"net/http"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"gotest.tools/assert"
+)
+
+func TestDecodeMux(t *testing.T) {
+	r, err := http.NewRequest("GET", "/foo", nil)
+	assert.NilError(t, err)
+
+	r = mux.SetURLVars(r, map[string]string{"foo": "bar"})
+
+	var v struct {
+		Foo string
+	}
+
+	err = DecodeRequest(r, &v)
+	assert.NilError(t, err)
+
+	assert.Equal(t, v.Foo, "bar")
+}
+
+func TestDecodeQuery(t *testing.T) {
+	r, err := http.NewRequest("GET", "/baz?foo=bar", nil)
+	assert.NilError(t, err)
+
+	var v struct {
+		Foo string
+	}
+
+	err = DecodeRequest(r, &v)
+	assert.NilError(t, err)
+
+	assert.Equal(t, v.Foo, "bar")
+}
+
+func TestDecodeBody(t *testing.T) {
+	body := []byte("{\"foo\":\"bar\"}")
+	r, err := http.NewRequest("POST", "/foo", bytes.NewBuffer(body))
+	assert.NilError(t, err)
+
+	var v struct {
+		Foo string
+	}
+
+	err = DecodeRequest(r, &v)
+	assert.NilError(t, err)
+
+	assert.Equal(t, v.Foo, "bar")
+}
+
+func TestDecodeIntoMap(t *testing.T) {
+	body := []byte("{\"foo\":\"bar\"}")
+	r, err := http.NewRequest("GET", "/baz?baz=qux", bytes.NewBuffer(body))
+	assert.NilError(t, err)
+
+	r = mux.SetURLVars(r, map[string]string{"quz": "cog"})
+
+	var v map[string]string
+
+	err = DecodeRequest(r, &v)
+	assert.NilError(t, err)
+
+	assert.Equal(t, v["foo"], "bar")
+	assert.Equal(t, v["baz"], "qux")
+	assert.Equal(t, v["quz"], "cog")
+}
+
+func TestDecodeComplexParamNames(t *testing.T) {
+	body := []byte("{\"animal_color\":\"black\"}")
+	r, err := http.NewRequest("GET", "/foo?house_name=Buckingham%20Palace", bytes.NewBuffer(body))
+	assert.NilError(t, err)
+
+	r = mux.SetURLVars(r, map[string]string{"favorite_number": "3"})
+
+	var v struct {
+		AnimalColor    string `json:"animal_color"`
+		HouseName      string `json:"house_name"`
+		FavoriteNumber int    `json:"favorite_number"`
+	}
+
+	err = DecodeRequest(r, &v)
+	assert.NilError(t, err)
+
+	assert.Equal(t, v.AnimalColor, "black")
+	assert.Equal(t, v.HouseName, "Buckingham Palace")
+	assert.Equal(t, v.FavoriteNumber, 3)
+}
--- libraries/go/taxi/router.go
+++ libraries/go/taxi/router.go
@@ -2,15 +2,9 @@ package taxi
 
 import (
 	"context"
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/mitchellh/mapstructure"
-
-	"github.com/jakewright/home-automation/libraries/go/oops"
 )
 
 const (
@@ -99,77 +93,10 @@ func (r *Router) log(format string, v ...interface{}) {
 	r.logFunc(format, v...)
 }
 
-// decodeRequest unmarshals URL parameters and the JSON body
-// of the given request into the value pointed to by v.
-func decodeRequest(r *http.Request, v interface{}) error {
-	// This does a load of reflection to unmarshal a map into the type of v
-	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
-		DecodeHook:       mapstructure.StringToTimeHookFunc(time.RFC3339),
-		WeaklyTypedInput: true,
-		Result:           v,
-
-		// Override the TagName to match the one used by the encoding/json package
-		// so users of this function only have to define a single tag on struct fields
-		TagName: "json",
-	})
-	if err != nil {
-		return oops.Wrap(err, oops.ErrInternalService, "failed to create decoder")
-	}
-
-	// Unmarshal route parameters
-	if err := decoder.Decode(mux.Vars(r)); err != nil {
-		return oops.Wrap(err, oops.ErrInternalService, "failed to decode route parameters")
-	}
-
-	// Query parameters come out as a map[string][]string so we loop through them all
-	// to remove the unnecessary slice if the parameter just has a single value
-	paramSlices := r.URL.Query()
-	params := map[string]interface{}{}
-	for key, value := range paramSlices {
-		switch len(value) {
-		case 0:
-			params[key] = nil
-		case 1:
-			params[key] = value[0]
-		default:
-			params[key] = value
-		}
-	}
-
-	// Unmarshal query parameters
-	if err := decoder.Decode(params); err != nil {
-		return oops.Wrap(err, oops.ErrInternalService, "failed to decode query parameters")
-	}
-
-	// If there's no body, return early
-	if r.Body == nil {
-		return nil
-	}
-
-	// Read the body of the request
-	defer func() { _ = r.Body.Close() }()
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return oops.Wrap(err, oops.ErrInternalService, "failed to read request body")
-	}
-
-	// If the body is empty, return early
-	if len(body) == 0 {
-		return nil
-	}
-
-	// Assume the body is JSON and unmarshal into v
-	if err := json.Unmarshal(body, v); err != nil {
-		return oops.Wrap(err, oops.ErrBadRequest, "failed to unmarshal request body")
-	}
-
-	return nil
-}
-
 func (r *Router) toHTTPHandler(handler Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		decoder := func(v interface{}) error {
-			return decodeRequest(req, v)
+			return DecodeRequest(req, v)
 		}
 
 		rsp, err := handler.ServeRPC(req.Context(), decoder)
