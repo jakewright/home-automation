ef1714312fe99c9c268f4de59ce2ab6de560bece
Jake Wright <jake@jakewright.net>
2020-09-21 20:55:53 +0100

    Implement color.RGBA wrapper

--- libraries/go/util/color.go
+++ libraries/go/util/color.go
@@ -1,11 +1,40 @@
 package util
 
 import (
+	"encoding/json"
 	"fmt"
 	"image/color"
 
 	"github.com/jakewright/home-automation/libraries/go/oops"
 )
+
+// RGB is an alias for color.RGBA but with JSON marshaling
+// methods that convert to & from hexadecimal color codes.
+type RGB color.RGBA
+
+// MarshalJSON converts the RGB color into a hex value (e.g. #FBEE13)
+func (r *RGB) MarshalJSON() ([]byte, error) {
+	if r == nil {
+		return json.Marshal(nil)
+	}
+
+	return []byte(ColorToHex(color.RGBA(*r))), nil
+}
+
+// UnmarshalJSON converts a hex value (e.g. #FF0000) to an RGB struct
+func (r *RGB) UnmarshalJSON(b []byte) error {
+	if r == nil {
+		return oops.InternalService("cannot unmarshal into nil receiver")
+	}
+
+	c, err := HexToColor(string(b))
+	if err != nil {
+		return err
+	}
+
+	r.R, r.G, r.B, r.A = c.R, c.G, c.B, c.A
+	return nil
+}
 
 // HexToColor turns a hexadecimal color code
 // (e.g. #FBEE13) into a color.RGBA.
