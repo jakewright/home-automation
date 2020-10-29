0baeddb0b3c308ad871778df736aa03d908a5ab9
Jake Wright <jake@jakewright.net>
2020-10-29 10:44:52 +0000

    Double quote to prevent globbing and word splitting
    https://github.com/koalaman/shellcheck/wiki/SC2086

--- bin/create_service.sh
+++ bin/create_service.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-mkdir ./services/${1}
+mkdir "./services/${1}"
 
 # -n Do not overwrite an existing file
 # -R Recurse into directory. Source must end in slash
 #    to copy the contents of the directory
-cp -nR ./tools/templates/service/ ./services/${1}/
+cp -nR ./tools/templates/service/ "./services/${1}/"
 
 # https://stackoverflow.com/a/50224830
 service_name_pascal=$(echo "${1}" | perl -nE 'say join "", map {ucfirst lc} split /[^[:alnum:]]+/')
 service_name_snake=$(echo "${1}" | tr '-' '_')
 
 # Loop through all the newly copied files & directories
-files=$(find ./services/${1})
+files=$(find "./services/${1}")
 for f in $files; do
     # Replace any instances of {{service_name_snake}} in the file (or directory) names
     new_name=$(echo $f | sed "s/{{service_name_snake}}/${service_name_snake}/")
@@ -29,11 +29,11 @@ for f in $files; do
 done
 
 # Loop through all of the files only and replace strings inside
-files=$(find ./services/${1} -type f)
+files=$(find "./services/${1}" -type f)
 for f in $files; do
-    sed -i '' "s/{{service_name_kebab}}/${1}/" $f
-    sed -i '' "s/{{service_name_pascal}}/${service_name_pascal}/" $f
-    sed -i '' "s/{{service_name_snake}}/${service_name_snake}/" $f
+    sed -i '' "s/{{service_name_kebab}}/${1}/" "$f"
+    sed -i '' "s/{{service_name_pascal}}/${service_name_pascal}/" "$f"
+    sed -i '' "s/{{service_name_snake}}/${service_name_snake}/" "$f"
 done
 
-go generate ./services/${1}/...
+go generate "./services/${1}/..."
